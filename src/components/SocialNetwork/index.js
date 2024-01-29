bd524a118610e5f066d8889e46182114f8543cbc
3963813 <swati.srivastava@lowes.com>
2024-01-29 15:52:51 +0530

    add Post component

--- src/components/SocialNetwork/index.js
+++ src/components/SocialNetwork/index.js
@@ -1,11 +1,13 @@
 import CreatePost from "./CreatePost/CreatePost";
 import LeftSidePanel from "./LeftSidePanel/LeftPanel";
+import Post from "./Post/Post";
 
 export function SocialNetwork () {
     return (
         <div>
             <LeftSidePanel />
             <CreatePost />
+            <Post />
         </div>
     )
 }
