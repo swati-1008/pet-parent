f54dec86e93f81da2e1806c5dedac7d0f2e20a4a
Swati Srivastava <swati100897@gmail.com>
2024-06-21 15:20:41 +0530

    added redux action and reducer for create post

--- FrontEnd/pet-parent/src/redux/reducers/index.js
+++ FrontEnd/pet-parent/src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import fetchSuggestionsReducer from './fetchSuggestionsReducer';
 import peopleYouMayKnowReducer from './peopleYouMayKnowReducer';
+import { createPostReducer } from './createPostReducer';
 
 const rootReducer = combineReducers({
     auth: authReducer, 
     suggestions: fetchSuggestionsReducer, 
     peopleYouMayKnow: peopleYouMayKnowReducer, 
+    createPost: createPostReducer, 
 });
 
 export default rootReducer;
