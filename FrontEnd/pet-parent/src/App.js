f583f5bda2cbc3b3a0bad6c79454b572cd12d01c
Swati Srivastava <swati100897@gmail.com>
2024-06-29 01:28:49 +0530

    added protected routes

--- FrontEnd/pet-parent/src/App.js
+++ FrontEnd/pet-parent/src/App.js
@@ -1,29 +1,39 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { SocialNetwork } from './components/SocialNetwork';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Authentication from './components/Authentication';
 import Home from './components/Home';
+import FurryFeeds from './components/FurryFeeds';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <Provider store={store}>
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/auth' element={<Authentication />} />
-            <Route path="/social" element={<SocialNetwork />} />
-            <Route path='/' element={<Home />} />
-            <Route path='/retail-stores' element={<Home />} />
-            <Route path='/chat-rooms' element={<Home />} />
-            <Route path='/vet-consultations' element={<Home />} />
-            <Route path='/blogs' element={<Home />} />
-            <Route path='/events' element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </Provider>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={
+            isAuthenticated ? <Navigate to='/home' /> : <Navigate to='/auth' />
+          } />
+          <Route path='/auth' element={<Authentication />} />
+          <Route path='/' element={<ProtectedRoute />}>
+          <Route path='/furry-feeds' element={<FurryFeeds />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/retail-stores' element={<Home />} />
+          <Route path='/chat-rooms' element={<Home />} />
+          <Route path='/vet-consultations' element={<Home />} />
+          <Route path='/blogs' element={<Home />} />
+          <Route path='/events' element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
 export default App;
+
+
+// TODO: 
+// 1. Do unit testing in both BE and FE
+// 2. Add Logout option
